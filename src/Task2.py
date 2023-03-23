5c07dc75c5b73956418b167bcde539b7249f1d7d
darshan152 <59141335+darshan152@users.noreply.github.com>
2023-03-23 13:21:10 +0800

    Add solution to Task 2

---
+++ src/Task2.py
@@ -0,0 +1,22 @@
+from google.cloud import bigquery
+
+
+client = bigquery.Client(project='de-take-home')
+
+# Query to find country which has the largest number of ports with a cargo wharf
+QUERY = ("""
+SELECT country, count(*) as port_count
+FROM `bigquery-public-data.geo_international_ports.world_port_index`
+WHERE cargo_wharf = TRUE
+GROUP BY country
+ORDER BY port_count DESC
+LIMIT 1
+""")
+
+# Create a BigQuery table with the query result
+table_id = "de-take-home.task_answers.country_with_most_cargo_wharf_ports"
+job_config = bigquery.QueryJobConfig(destination=table_id, write_disposition="WRITE_TRUNCATE")
+query_job = client.query(QUERY, job_config=job_config)
+print("Processing query")
+query_job.result()
+print("Completed")
